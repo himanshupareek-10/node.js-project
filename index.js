2f11c7bf5006b62b5164d333951d31620291628e
Himanshu Pareek <pareek10.himanshu@gmail.com>
2023-02-13 10:28:51 +0530

    refactored-code

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors');
 const express = require("express");
-
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const app = express();
+
+mongoose.connect(`mongodb://localhost:27017/node_project`);
+
 
 const get_router = require("./routes/get-route");
 const post_router = require("./routes/post-route");
